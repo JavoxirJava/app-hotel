64cbc2f5f4787fd75acdb5db9865f608ff97aecc
Network_Attack <javoxir8177@gmail.com>
2022-07-22 19:18:28 +0500

    Javoxir 2-push

--- hotel_register/src/main/java/uzb/hotel_register/service/RoomService.java
+++ hotel_register/src/main/java/uzb/hotel_register/service/RoomService.java
@@ -16,55 +16,50 @@ public class RoomService {
     @Autowired
     RoomRepository roomRepository;
 
-    public ApiResponse addRoom(ReqRoom reqRoom){
-        if(!roomRepository.existsByRoomNumberEquals(reqRoom.getRoomNumber())){
+    public ApiResponse addRoom(ReqRoom reqRoom) {
+        if (!roomRepository.existsByRoomNumberEquals(reqRoom.getRoomNumber())) {
             Room room = new Room();
             room.setRoomNumber(reqRoom.getRoomNumber());
-            room.setCategoryRoomName(reqRoom.getCategoryRoomName());
-            room.setRoomSize(reqRoom.getRoomSiz());
             room.setPriceDay(reqRoom.getPriceDay());
             room.setPriceTime(reqRoom.getPriceTime());
             roomRepository.save(room);
-            return new ApiResponse("Successfully saved" ,true);
-        }
-            return new ApiResponse("This room already exist", false);
-    }
-
-    public ApiResponse editRoom(Integer id, ReqRoom reqRoom){
-        if(!roomRepository.existsByRoomNumberEquals(reqRoom.getRoomNumber())){
-            Room room = roomRepository.findById(id).get();
-            room.setRoomNumber(reqRoom.getRoomNumber());
-            room.setCategoryRoomName(reqRoom.getCategoryRoomName());
-            room.setRoomSize(reqRoom.getRoomSiz());
-            room.setPriceDay(reqRoom.getPriceDay());
-            room.setPriceTime(reqRoom.getPriceTime());
-            roomRepository.save(room);
-            return new ApiResponse("Successfully edit" ,true);
+            return new ApiResponse("Successfully saved", true);
         }
         return new ApiResponse("This room already exist", false);
     }
 
-    public ApiResponse deleteRoom(Integer id){
+    public ApiResponse editRoom(Integer id, ReqRoom reqRoom) {
+        if (!roomRepository.existsByRoomNumberEquals(reqRoom.getRoomNumber())) {
+            Room room = roomRepository.findById(id).get();
+            room.setRoomNumber(reqRoom.getRoomNumber());
+            room.setPriceDay(reqRoom.getPriceDay());
+            room.setPriceTime(reqRoom.getPriceTime());
+            roomRepository.save(room);
+            return new ApiResponse("Successfully edit", true);
+        }
+        return new ApiResponse("This room already exist", false);
+    }
+
+    public ApiResponse deleteRoom(Integer id) {
         roomRepository.deleteById(id);
         return new ApiResponse("Successfully delete", true);
     }
 
-    public List<ResRoom> getRoomList(){
+    public List<ResRoom> getRoomList() {
         List<ResRoom> resRoomList = new ArrayList<>();
         List<Room> roomList = roomRepository.findAll();
         Integer tr = 0;
-        for(Room room : roomList){
+        for (Room room : roomList) {
             tr++;
             ResRoom resRoom = new ResRoom();
             resRoom.setTr(tr);
             resRoom.setId(room.getId());
             resRoom.setRoomNumber(room.getRoomNumber());
-            resRoom.setCategoryRoomName(room.getCategoryRoomName());
             resRoom.setRoomSiz(room.getRoomSize());
             resRoom.setPriceDay(room.getPriceDay());
             resRoom.setPriceTime(room.getPriceTime());
             resRoomList.add(resRoom);
         }
-            return resRoomList;
+        return resRoomList;
     }
 }
